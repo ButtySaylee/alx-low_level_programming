5ebac11d633d26149b4280bdc8f87d4d9d3f155d
butty <nbs917740@gmail.com>
2021-08-26 19:20:31 -0700

    printed a string using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - printed strings char using puts
+ * included a new line
+ * Return: 0 upon after successful execution
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
