c4216cba192874d3cba5c9299e235fb0d81ec29b
butty <nbs917740@gmail.com>
2021-08-27 09:44:32 -0700

    printed a string usng printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print strings using printf
+ * along with a new line
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
